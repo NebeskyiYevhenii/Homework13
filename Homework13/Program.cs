9d5959914f10f0dbb2c540185b759d76c22d1918
NebeskyiYevhenii <evheniy.slav@gmail.com>
2020-09-20 16:03:02 +0300

    A singly linked implementation class for a singly linked list. System.Collections.IEnumerable interface

--- Homework13/Program.cs
+++ Homework13/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,59 @@ namespace Homework13
     {
         static void Main(string[] args)
         {
+           // Console.ReadKey();
         }
+    }
+
+
+    public class QueueItem<T>
+    {
+        public T Data { get; set; }
+        public QueueItem<T> Next { get; set; }
+
+        public QueueItem(T data)
+        {
+            Data = data;
+        }
+    }
+
+    public class Queue<T> : IEnumerable<T>  // односвязный список
+    {
+        QueueItem<T> head; // головной/первый элемент
+        QueueItem<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            QueueItem<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        public void AddItem(T data)
+        {
+            QueueItem<T> queueItem = new QueueItem<T>(data);
+
+            if (head == null)
+            {
+                head = queueItem;
+            }  
+            else
+            {
+                tail.Next = queueItem;
+            }
+
+            tail = queueItem;
+            count++;
+        }
+
     }
 }
